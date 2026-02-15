f20d71e32700bfd8744c395ca693d3b7f7a2c3b0
MeadowSmith <89278981+GittemGittem@users.noreply.github.com>
2026-02-15 15:56:55 -0600

    Uploading branch this branch uses with statement to create catalogues and enums instead

--- catalogue/catalogue.py
+++ catalogue/catalogue.py
@@ -1,122 +1,55 @@
-from typing import Any
 
-class __catalogue(type):
-    __root_class = None
+class Group():
     
-    class Item:
-        type ItemID = int
-        type ItemName = str
-        type ItemValue = Any
+    class __Type:
+                
+        def __init__(self, step):
+            self.next_id = 0
+            self.step = step
         
-        def __init__(self, id, name, value, parent):
-            self.__id = id
-            self.__name = name
-            self.__value = value
-            self.__parent = parent
+        def __auto__(self, id=None):
+            if id is None:
+                id = self.next_id
+            self.next_id = id + self.step
+            return id
+        
+        def __enter__(self):
+            return self.__auto__, self
+        def __exit__(self, exc_type, exc_message, exc_traceback): pass
+    class Enum(__Type): pass
+    
+    class Catalogue(__Type):
+        def __init__(self, step):
+            super().__init__(step)
+            self.items = {}
+        
+        def __auto__(self, item, id=None):
+            id = super().__auto__(id)
+            self.items[id] = item
+            return id
+        
+        def __getitem__(self, id=None):
+            if id is None:
+                try:
+                    id = next(reversed(self.items))
+                except StopIteration:
+                    raise KeyError("No items available!")
+            return self.items[id]
+        
+        def __iter__(self):
+            for id in self.items:
+                yield id
+        def __contains__(self, id:int):
+            return self.items[id]
             
-        @property
-        def ID(self) -> ItemID:
-            return self.__id
-        @property
-        def PARENT(self) -> "Catalogue":
-            return self.__parent
-        @property
-        def NAME(self) -> ItemName:
-            return self.__name
-        @property
-        def VALUE(self) -> ItemValue:
-            return self.__value
-        
-        def __repr__(self) -> str:
-            match self.VALUE:
-                case None:
-                    value = ""
-                case anything_else:
-                    value = f" -> {anything_else}"                    
-            return f"|{self.__parent.__name__}.{self.NAME}/{self.__id}{value}|"
-        
-        def __eq__(self, other) -> bool:
-            return (self.__id == other.ID)
-        def __ne__(self, other) -> bool:
-            return not self.__eq__(other)
-        
-        def __gt__(self, other) -> bool:
-            return self.__id > other.ID
-        def __lt__(self, other) -> bool:
-            return self.__id < other.ID
-        
-        def __le__(self, other) -> bool:
-            return self.__lt__(other) or self.__eq__(other)
-        def __ge__(self, other) -> bool:
-            return self.__gt__(other) or self.__eq__(other)
-        
-    def __new__(meta, name, bases, namespace, **keywords) -> "Catalogue":
-        if meta.__root_class is None:
-            meta.__root_class = super().__new__(meta, name, bases, namespace, **keywords)
-            return meta.__root_class
-        else:
-            base = {
-                        "next_id" : 0,
-                        "instances":{},
-                        "ids":{},
-                        "step":1
-                    }
-            for key, val in namespace.items():
-                if not (key.isupper() and not (key.startswith("__") and key.endswith("__"))):
-                    base[key] = val
-                    
-                    
-            cls = super().__new__(meta, name, bases, base, **keywords)
-            for name, value in namespace.items():
-                if (not name.startswith('__') and not name.endswith('__')) and name.isupper():
-                    if isinstance(value, tuple):
-                        item, id = value
 
-                        if not isinstance(id, int):
-                            id = None
-                    elif isinstance(value, dict):
-                        item = value.get("item", None) or value.get("ITEM")
-                        id = value.get("id", None) or value.get("ID")
-                    else:
-                        item, id = value, None
-                    meta.__call__(cls, name, item, id)
-            return cls
-    
-    def __call__(cls, name, item, id=None) -> Item:
-        
-        if cls is cls.__class__.__root_class:
-            raise SyntaxError("Cannot use Catalogue class as a catalogue")
-        if id is None:
-            id = cls.next_id
-        cls.next_id = id + cls.step
-        
-        if id in cls.instances:
-            instance = cls.instances[id]
-            instance._Item__value = item
-            instance._Item__name = name
-        else:
-            instance = cls.__class__.Item(id, name, item, cls)
-            cls.instances[id] = instance
-        cls.ids[name] = id
-        return instance
+with Group.Enum(step=1) as (auto, enum):
+    X = auto()
 
-    def __getattr__(cls, name):
-        if name in cls.ids:
-            return cls.instances[cls.ids[name]]
-        else:
-            raise KeyError(f"No item '{name}' in {cls.__name__}")
+print(X)
 
-
-class Catalogue(metaclass = __catalogue):
-    def __init_subclass__(cls, step=1):
-        cls.step = step
-
-        
-if __name__ == "__main__":
-    class Colors(Catalogue):
-        RED = {"ITEM":(0, 0, 0), "ID":0}
-        BLUE = (0, 0, 255), 0
-
-    print(Colors.RED == Colors.BLUE)
-
-    print(Colors.RED.PARENT)
+with Group.Catalogue(step = 1) as (auto, tileset):
+    GRASS = auto("grass", 10)
+    BRICK = auto("bricks")
+print(GRASS)
+print(tileset[BRICK])
