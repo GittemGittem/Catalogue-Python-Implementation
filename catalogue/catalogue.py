867ee30b2114f49d24a96a855b21bab4106aefad
MeadowSmith <89278981+GittemGittem@users.noreply.github.com>
2026-02-16 12:36:34 -0600

    I think this might be the final form until I learn c and cpython guys I might upload an examples.py as well

--- catalogue/catalogue.py
+++ catalogue/catalogue.py
@@ -1,117 +1,111 @@
-
-class Item:
-    __slots__ = ("__id", "NAME", "VALUE", "PARENTS", "PARENT")
-    def __init__(self, id:int):
-
-        self.__id = id
-        self.PARENT = None
-        self.PARENTS = ()
-        self.VALUE = None
-        self.NAME = None
-    
-
-    def __eq__(self, other):
-        return self.ID == other.ID
-    def __ne__(self, other):
-        return not self.__eq__(other)
-    def __gt__(self, other):
-        return self.ID > other.ID
-    def __lt__(self, other):
-        return self.ID < other.ID
-    def __ge__(self, other):
-        return self.__gt__(other) or self.__eq__(other)
-    def __le__(self, other):
-        return self.__lt__(other) or self.__eq__(other)
-
-    def __matmul__(self, other): # @
-        return len(set(self.PARENTS) & set(other.PARENTS)) > 0
-    
-    def __and__(self, other): # &
-        return self.PARENT == other.PARENT and self.PARENT is not None
-
-    def __xor__(self, other): # ^
-        return self.VALUE is other.VALUE
-    
-    @staticmethod
-    def is_id_name(name:str):
-        return (not name.startswith('__') and not name.endswith('__')) and name.isupper()
-    
-    def __repr__(self):
-        parent_name = getattr(self.PARENT, "name", None)
-        name = self.NAME
-        id = self.__id
-        value = self.VALUE
-        
-        if parent_name is not None: parent_name = str(parent_name) + (":" if name is None else ".")
-        if name is not None: name = name + "/"
-        if value is not None: value = f" -> '{value}'"
-        
-        
-        return f"|{parent_name or ""}{name or ""}{id}{value or ""}|"
-    
-    def __getitem__(self, value):
-        if isinstance(value, Item):
-            self.VALUE = value.VALUE
-            self.NAME = value.NAME
-            self.PARENTS = (*self.PARENTS, *value.PARENTS)
-        else:
+class ID:
+        __slots__ = ("__id", "VALUE")
+        def __init__(self, id, value=None):
+            self.__id = id
             self.VALUE = value
-        return self
-                
-    @property
-    def ID(self):
-        return self.__id
-
-class Catalogue:
+        @property
+        def ID(self):
+            return self.__id
+        
+        def __repr__(self):
+            value = getattr(self, "VALUE", None)
+            if value is not None and value != "": value = " -> " + str(value)
+            return f"|ID:{self.ID}{value or ''}|"
+        
+        def __eq__(self, other):
+            match other:
+                case other if isinstance(other, ID):
+                    return self.ID == other.ID
+                case other if isinstance(other, int):
+                    return self.ID == other
+                case _:
+                    raise TypeError(f"Cannot compare '{self.__class__.__name__}' with '{other.__class__.__name__}'")
+        def __ne__(self, other):
+            return not self.__eq__(other)
+        def __gt__(self, other):
+            match other:
+                case other if isinstance(other, ID):
+                    return self.ID > other.ID
+                case other if isinstance(other, int):
+                    return self.ID > other
+                case _:
+                    raise TypeError(f"Cannot compare '{self.__class__.__name__}' with '{other.__class__.__name__}'")
+        def __lt__(self, other):
+            match other:
+                case other if isinstance(other, ID):
+                    return self.ID < other.ID
+                case other if isinstance(other, int):
+                    return self.ID < other
+                case _:
+                    raise TypeError(f"Cannot compare '{self.__class__.__name__}' with '{other.__class__.__name__}'")
+        def __ge__(self, other):
+            return self.__gt__(other) or self.__eq__(other)
+        def __le__(self, other):
+            return self.__lt__(other) or self.__eq__(other)
+        
+        def __and__(self, other):
+            if self.VALUE is not None:
+                if isinstance(other, ID):
+                    return self.VALUE == other.VALUE
+                return self.VALUE == other
+            return False
+            
+class Registry:
+    
+    def seal(self):
+        self.__sealed = True
+    def unseal(self):
+        self.__sealed = False
+        
+    def toggle_seal(self):
+        self.__sealed = not self.__sealed
+    
+    def is_sealed(self):
+        return self.__sealed
+    
+    
+    def __enter__(self):
+        self.unseal()
+        return self.register
+    
+    def __exit__(self, *exc_args):
+        self.seal()
+    
     def __init__(self, name=None, step=1):
         self.step = step
         self.next_id = 0
         self.name = name
-        
-        self.items = {}
+        self.__sealed = False
+    
         self.ids = {}
     
-    def __auto__(self, id=None):
+    def __auto__(self, id):
         if id is None:
             id = self.next_id
         self.next_id = id + self.step
         return id
     
-    def __create_item(self, id=None):
-        if id in self.items:
-            item = self.items[id]
-        else:
-            item = Item(self.__auto__(id))
-        item.PARENT = self
-        if self not in item.PARENTS:
-            item.PARENTS = (*item.PARENTS, self)
-        elif item.PARENTS[-1] != self:
-            item.PARENTS = (*(parent for parent in item.PARENTS if parent is not self), self)
-        return item
-    
-    def __getattr__(self, name):
-        if name == "Item":
-            return self.__create_item
-        elif Item.is_id_name(name):
-            return self.items[self.ids[name]]
-        else:
-            raise AttributeError(f"No attribute {name} in {self}")
+    def register(self, value:object=None, id:int=None) -> ID:
+        if isinstance(value, ID):
+            raise TypeError("Cannot set ID's value to an ID")
+        if self.is_sealed(): # but if it has been locked, it can no longer be used unless context managered again
+            name = getattr(self, 'name', None)
+            if name is not None and name != "": name = f" '{name}'"
+            raise SyntaxError(f"Cannot use a Registry{name or ""} while it is sealed")
 
-    def __setattr__(self, name, value):
-        match name, value:
-            case name, value if name == "Item":
-                raise ValueError("Cannot set Item on catalogue")
-            case name, value if Item.is_id_name(name):
-                if isinstance(value, Item):
-                    value.NAME = name
-                    value.PARENT = self
-                    if self not in value.PARENTS:
-                        value.PARENTS = (*value.PARENTS, self)
-                    elif value.PARENTS[-1] != self:
-                        value.PARENTS = (*(parent for parent in value.PARENTS if parent is not self), self)
-                    self.items[value.ID] = value
-                    self.ids[name] = value.ID
-                else:
-                    raise ValueError(f"Can only set item to 'Item' type not '{value.__class__.__name__}'")
-            case name, value:
-                super().__setattr__(name, value)
+        id = self.__auto__(id)
+        if id in self.ids:
+            instance = self.ids[id]
+        else:
+            instance = ID(id)
+            self.ids[id] = instance
+        instance.VALUE = value
+        return instance
+    def __getitem__(self, id):
+        return self.ids[id]
+    
+
+with Registry() as auto:
+    X = auto("hello", 10)
+    Z = auto("hello", 10)
+Y = ID(Z.ID, "hello")
