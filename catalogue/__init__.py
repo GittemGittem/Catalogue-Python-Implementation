79a4c566fc1e1130cd5c2bc3e4e396f63ef06d8e
MeadowSmith <89278981+GittemGittem@users.noreply.github.com>
2026-02-16 13:36:37 -0600

    fixed ID __hash__ function

--- catalogue/__init__.py
+++
@@ -1,2 +0,0 @@
-from catalogue.catalogue import Catalogue
-
--- test.py
+++
@@ -1,6 +0,0 @@
-from catalogue import Catalogue
-
-with Catalogue() as Colors:
-    Colors.RED = 1, 2, 3, Catalogue.ID(10)
-    
-print(Colors.RED)
